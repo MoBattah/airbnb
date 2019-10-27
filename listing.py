cab99ba0c0b44387e09065516d17ebeff0e3b924
Mo Battah <mohhandb@hotmail.com>
2019-10-27 02:33:43 -0700

    Create listing.py

---
+++ listing.py
@@ -0,0 +1,8 @@
+class Listing:
+    def __init__(self, price, beds, bathrooms, neighborhood, name, nightly_rate):
+        self.price = price
+        self.beds = beds
+        self.bathrooms = bathrooms
+        self.neighborhood = neighborhood
+        self.name = name
+        self.nightly_rate = nightly_rate
