e65e6e4b074fe63e7e981cd8ada6c01e93de11e9
Mo Battah <mohhandb@hotmail.com>
2019-11-13 15:20:42 -0800

    Added attributes

--- listing.py
+++ listing.py
@@ -1,8 +1,12 @@
 class Listing:
-    def __init__(self, price, beds, bathrooms, neighborhood, name, nightly_rate):
+    def __init__(self, amenities, fake_price, price, monthly_rate, beds, bathrooms, neighborhood, name, nightly_rate, listing_id):
         self.price = price
         self.beds = beds
         self.bathrooms = bathrooms
         self.neighborhood = neighborhood
         self.name = name
         self.nightly_rate = nightly_rate
+        self.id = listing_id
+        self.fake_price = fake_price
+        self.amenities = amenities
+        self.monthly_rate = monthly_rate
