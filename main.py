a1410cf8d071c3d513d184043ca1461ec6c3b3fe
Mo Battah <mohhandb@hotmail.com>
2019-11-13 15:20:22 -0800

    Updated calculation

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import airbnb
 import listing
 from datetime import datetime
 
-date_1 = '2019-11-10'
-date_2 = '2019-11-22'
+### PARAMETERS
+date_1 = '2019-12-06'
+date_2 = '2019-12-23'
+location = 'Oceanside, CA'
+private_entrance = True
 api = airbnb.Api()
 
 
 def main():
     days = days_between(date_1, date_2)
-    search = api.get_homes('Carlsbad, CA', checkin='2019-11-10',checkout='2019-11-20', items_per_grid=100)
+    search = api.get_homes(location, checkin=date_1,checkout=date_2, items_per_grid=100)
     for x in search['explore_tabs'][0]['sections']:
         if x['result_type'] == 'listings':
             listings = x['listings']
@@ -17,6 +20,8 @@ def main():
 
     for x in listings:
         real_price = x['pricing_quote']['price']['total']['amount']
+        fake_rate = x['pricing_quote']['rate']['amount']
+        fake_price = int(fake_rate) * int(days)
         nightly_rate = int(real_price / days)
         listing_obj = x['listing']
         if 'neighborhood' in listing_obj:
@@ -24,24 +29,34 @@ def main():
         else:
             neighborhood = listing_obj['city']
 
-        a_listing = listing.Listing(price=real_price, nightly_rate=nightly_rate, name=listing_obj['name'],
+        amenities = listing_obj['amenity_ids']
+        if 'beds' in listing_obj:
+            pass
+        else:
+            listing_obj['beds'] = 1
+        monthly_rate = nightly_rate * 30
+        a_listing = listing.Listing(amenities = amenities, fake_price = fake_price, price=real_price, nightly_rate=nightly_rate,monthly_rate=monthly_rate, name=listing_obj['name'],
                                     bathrooms=listing_obj['bathrooms'], beds=listing_obj['beds'],
                                     neighborhood=neighborhood, listing_id=listing_obj['id'])
         list_of_homes.append(a_listing)
+        print(real_price, a_listing.name)
 
-    sorta = sorted(list_of_homes, key=lambda listing:listing.nightly_rate)
+    sorta = sorted(list_of_homes, key=lambda listing:listing.monthly_rate)
     for x in sorta:
-        print("Nightly rate: $", x.nightly_rate, "Name: ", x.name, " Area: ", x.neighborhood, " Total price: ", x.price)
-
-
-
-
+        if 57 in x.amenities:
+                print( "Name: ", x.name, " Area: ", x.neighborhood,"** Private Entrace **\n Nightly rate: $", x.nightly_rate,"\n Monthly rate: $", x.monthly_rate, "\n Total price: ", x.price, " Advertised Price: ", x.fake_price, "Difference :", x.fake_price - x.price, "\n Link: ", "https://airbnb.com/rooms/"+str(x.id))
+        else:
+            print("Name: ", x.name, " Area: ", x.neighborhood, "\n Nightly rate: $", x.nightly_rate,
+                  "\n Monthly rate: $", x.monthly_rate, "\n Total price: ", x.price, " Advertised Price: ",
+                  x.fake_price, "Difference :", x.fake_price - x.price, "\n Link: ",
+                  "https://airbnb.com/rooms/" + str(x.id))
 
 
 """
 Print details about the request
 
  """
+#print("Request \nMarket: ", search['metadata']['geography']['market'])
 
 def days_between(d1, d2):
     d1 = datetime.strptime(d1, "%Y-%m-%d")
